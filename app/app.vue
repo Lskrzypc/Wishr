869e30554279f1c9c2a30d6dce655731137303f6
Lskrzypc <louis.skrzypczak@student.junia.com>
2025-10-30 14:40:18 +0100

    feat: add Tailwind CSS

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+const toaster = { expand: true };
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UApp :toaster="toaster">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </UApp>
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+const text = 'Hello World';
+</script>
+<template>
+  <div class="w-full">{{ text }}</div>
+</template>
